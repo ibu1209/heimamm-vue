f14d742026e6cf058058206dd3041b60ce88cc01
ibu <1427068253@qq.com>
2020-03-16 16:23:08 +0800

    完成项目结构搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入router
+import router from './router'
+//导入element-ui
+import Element from 'element-ui'
+//导入element-ui的样式
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(Element);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
